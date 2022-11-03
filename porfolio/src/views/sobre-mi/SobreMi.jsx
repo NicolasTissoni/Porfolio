175d5d4dbb1a675dfb35b221e6213f186a6d7281
Nico <nicotissoni12@gmail.com>
2022-11-03 19:06:52 -0300

    agregado enlace a CV

--- porfolio/src/views/sobre-mi/SobreMi.jsx
+++ porfolio/src/views/sobre-mi/SobreMi.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './sobreMi.scss';
 import FotoPor from '../../images/FotoPor.jpg';
+import CV from '../../documents/CV NICO.pdf';
 
 const SobreMi = () => {
   return (
@@ -12,7 +13,7 @@ const SobreMi = () => {
         <div className='sobre-Mi-Iconos-Container'>
           <a href="https://www.linkedin.com/in/nicolas-tissoni-9b4838207/" target='_blank'><i className="fa-brands fa-linkedin"></i></a>
           <a href="https://github.com/NicolasTissoni" target='_blank'><i className="fa-brands fa-github"></i></a>
-            <a href="">CV</a>
+            <a target='_blank' href={CV}>CV</a>
         </div>
       </div>
     </div>
