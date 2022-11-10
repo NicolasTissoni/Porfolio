3092f8374e932a091717891f3a46a6b87d6c9456
Nico <nicotissoni12@gmail.com>
2022-11-10 08:50:12 -0300

    Agregado Proyecto-Peliculas

--- src/views/proyectos/Proyectos.jsx
+++ src/views/proyectos/Proyectos.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './proyectos.scss';
 import Ecommerce from '../../images/E-Commerce.png';
+import Peliculas from '../../images/Peliculas-Proyecto.png';
 
 const Proyectos = () => {
   return (
@@ -13,13 +14,22 @@ const Proyectos = () => {
               <div className="overlay">
                 <div className="overlay-text">
                   <h2>Tecnologias Usadas:</h2>
-                  <p>React, FireBase, Sass, Node js</p>
+                  <p>React, FireBase, Sass, Node.js</p>
                 </div>
               </div>
             </a>
         </div>
-        <div className='proyectos-Proximamente'>
-            <h3>Proximamente...</h3>
+        <div className='proyectos-Info-Container'>
+            <img src={Peliculas} alt="Proyecto-Peliculas" />
+            <h3 className='proyectos-tecnologias-usadas'>E-Commerce</h3>
+            <a href="https://peliculasproyecto.netlify.app" target='_blank'>
+              <div className="overlay">
+                <div className="overlay-text">
+                  <h2>Tecnologias Usadas:</h2>
+                  <p>React, Sass, Node.js</p>
+                </div>
+              </div>
+            </a>
         </div>
         <div className='proyectos-Proximamente'>
             <h3>Proximamente...</h3>
