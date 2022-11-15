f0a1c19a56de89cefbbe2395be55d70d9549dbc0
Nico <nicotissoni12@gmail.com>
2022-11-15 20:38:10 -0300

    Agregado Proyecto Login

--- src/views/proyectos/Proyectos.jsx
+++ src/views/proyectos/Proyectos.jsx
@@ -3,6 +3,8 @@ import './proyectos.scss';
 import Ecommerce from '../../images/E-Commerce.png';
 import Peliculas from '../../images/Peliculas-Proyecto.png';
 import Kanban from '../../images/Kanban.png';
+import Login from '../../images/Login-Proyecto.png';
+
 const Proyectos = () => {
   return (
     <div className='proyectos-Container' id='3'>
@@ -39,6 +41,18 @@ const Proyectos = () => {
                 <div className="overlay-text">
                   <h2>Tecnologias Usadas:</h2>
                   <p>React, Sass, Node.js</p>
+                </div>
+              </div>
+            </a>
+        </div>
+        <div className='proyectos-Info-Container'>
+            <img src={Login} alt="Proyecto-Login" />
+            <h3 className='proyectos-tecnologias-usadas'>E-Commerce</h3>
+            <a href="https://proyectologinnt.netlify.app" target='_blank'>
+              <div className="overlay">
+                <div className="overlay-text">
+                  <h2>Tecnologias Usadas:</h2>
+                  <p>React, Sass, Node.js, FireBase</p>
                 </div>
               </div>
             </a>
