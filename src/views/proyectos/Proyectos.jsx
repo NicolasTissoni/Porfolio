fdcb61efd156e66eac7dd1a24f1c63f29301f55d
Nico <nicotissoni12@gmail.com>
2022-11-15 09:45:30 -0300

    Agregado el proyecto Kanban

--- src/views/proyectos/Proyectos.jsx
+++ src/views/proyectos/Proyectos.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './proyectos.scss';
 import Ecommerce from '../../images/E-Commerce.png';
 import Peliculas from '../../images/Peliculas-Proyecto.png';
-
+import Kanban from '../../images/Kanban.png';
 const Proyectos = () => {
   return (
     <div className='proyectos-Container' id='3'>
@@ -31,8 +31,17 @@ const Proyectos = () => {
               </div>
             </a>
         </div>
-        <div className='proyectos-Proximamente'>
-            <h3>Proximamente...</h3>
+        <div className='proyectos-Info-Container'>
+            <img src={Kanban} alt="Proyecto-Kanban" />
+            <h3 className='proyectos-tecnologias-usadas'>E-Commerce</h3>
+            <a href="https://kanbanproyecto.netlify.app" target='_blank'>
+              <div className="overlay">
+                <div className="overlay-text">
+                  <h2>Tecnologias Usadas:</h2>
+                  <p>React, Sass, Node.js</p>
+                </div>
+              </div>
+            </a>
         </div>
     </div>
   )
